28f891240cd69e9096216db5a3e050bb87061d29
VannPellegrini <vanessapellegrini@hotmail.com>
2017-07-13 20:37:24 -0400

    index menu update

--- assets/js/validacion-inicio.js
+++ assets/js/validacion-inicio.js
@@ -2,10 +2,15 @@
 $(document).ready(function(){
 	$("#submitButton").click(function(e){
 		console.log("hola");
+
 		function isEmail(email) {
   		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
  		 return regex.test(email);
 		};
+		/*function isPass(pass) {
+  		var regex = /^([0-9])/;
+ 		 return regex.test(pass);
+		};*/
 
 		var error = "";
 		var vacio = "";
@@ -25,15 +30,20 @@ $(document).ready(function(){
 			error += "<p>El siguiente campo se encuentra vacío" + vacio;
 		}
 
-		if ($("#pass").val().length != 8) {
+		if ($("#pass").val().length > 8) {
 			e.preventDefault();
-			error += "<p> La contraseña no es válida, favor ingresar 8 carácteres</p>";
+			error += "<p> La contraseña no es válida, favor ingresar hasta 8 carácteres</p>";
 		}
 
 		if (isEmail($("#email").val()) == false ) {
 			e.preventDefault();
-			error += "<p>Your Email address is not valid</p>";
+			error += "<p>El email es incorrecto</p>";
 		}
+
+		/*if (isPass($("#pass").val()) == false ) {
+			e.preventDefault();
+			error += "<p>La contraseña es incorrecta</p>";
+		}*/
 
 		if (error != "") {
 			$("#error").html(error);
