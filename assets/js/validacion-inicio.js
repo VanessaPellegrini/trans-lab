f340ed2b1daa1080c45e2a9c21ce37251d5983d4
VannPellegrini <vanessapellegrini@hotmail.com>
2017-07-14 08:52:12 -0400

    index inicio totalmente funcional, se realizó cambios en la validación a través de expresiones regurales en donde solo solicita que se ingresen números, además se implementa localstorage, que guarda email en el index de inicio y luego lo imprime en el index-perfil

--- assets/js/validacion-inicio.js
+++ assets/js/validacion-inicio.js
@@ -1,17 +1,25 @@
 //console.log("funciona");
 $(document).ready(function(){
+	$("#caja-imprimir").html(localStorage.getItem("emailLocal"));
+	//$("#email-box").append('<p>'+localStorage.getItem("localEmail")+'<p>');
+	/*var mostrarEmail = function(){
+		//show items saved in local storage
+  		return $("#email").value(localStorage.getItem("localEmail"));
+	}
+	console.log(mostrarEmail);*/
+
 	$("#submitButton").click(function(e){
-		console.log("hola");
+		console.log("welcome to the menu");
 
 		function isEmail(email) {
   		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
  		 return regex.test(email);
 		};
-		/*function isPass(pass) {
-  		var regex = /^([0-9])/;
- 		 return regex.test(pass);
-		};*/
-
+		function isPass(pass) {
+  			var regex = /^[0-9]+$/;
+ 		 	return regex.test(pass);
+		};
+		console.log(isPass() + " FALSE = NO ES UN NUMERO, TRUE = EUREKA");
 		var error = "";
 		var vacio = "";
 
@@ -39,11 +47,16 @@ $(document).ready(function(){
 			e.preventDefault();
 			error += "<p>El email es incorrecto</p>";
 		}
+		if (isEmail($("#email").val()) == true ) {
+			e.preventDefault();
+			localStorage.setItem("emailLocal", $("#email").val());
+		}
+		console.log(localStorage)
 
-		/*if (isPass($("#pass").val()) == false ) {
+		if (isPass($("#pass").val()) == false ) {
 			e.preventDefault();
 			error += "<p>La contraseña es incorrecta</p>";
-		}*/
+		}
 
 		if (error != "") {
 			$("#error").html(error);
@@ -51,6 +64,8 @@ $(document).ready(function(){
 			$("#error").hide();
 			e.preventDefault();
 			window.location = "index-menu.html";
+
 		}
 	});
+	//console.log(localStorage)
 });
